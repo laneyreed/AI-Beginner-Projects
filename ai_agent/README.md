d44dfdb0a520d442a84f5dacf4a0ac48fa04d66a
Shanreed25 <tralainereed@gmail.com>
2025-12-07 21:49:22 -0800

    added tool

--- ai_agent/README.md
+++ ai_agent/README.md
@@ -2,18 +2,17 @@
 **A AI Agent is a software program that uses artificial intelligence techniques to perform tasks autonomously**
 - can interact with users, make decisions, and learn from their environment
 - are used in various applications such as virtual assistants, chatbots, and recommendation systems
+- LLM acts as the brain
+- A AI Agent is a chatbot that can perform tasks using tools and language models
 
 ---
 
 **This program will ask the user for some input then send the input to the agent. When the response comes back from the agent, it is streammed out and printed to the console**
-
-
-
 - created with `uv`: `uv init .`
 - installed `uv add langgraph langchain python-dotenv langchain-openai`
-
 - Open AI API Key from: https://platform.openai.com/api-keys
     - https://platform.openai.com/docs/api-reference/introduction
+- has a simple tool that the agent can use so that it can do something beyond just a simple response
 
 #### 
 - `langchain`: high level framework that allows us to build AI Applications
@@ -24,11 +23,7 @@
 
 
 
-- a simple tool that the agent can use so that it can do something beyond just a simple response
-- we can make any kind of tool such as a tool to call a API
-- calculator
 
 
 
-- LLM acts as the brain
-- A AI Agent is a chatbot taht can perform tasks using tools and language models
+
--- ai_agent/main.py
+++ ai_agent/main.py
@@ -6,18 +6,14 @@ from langchain.agents import create_agent
 
 from dotenv import load_dotenv
 
-load_dotenv()  # Load environment variables from .env file
+load_dotenv() 
 
-# toll is a external service that the agent can use to perform specific tasks
-#@tool decorator to define a tool for the agent
-# the decorator takes care of registering the tool with the agent and making it available for use
-#
+
 @tool
 def calculator_tool(x: float, y: float) -> str:
-    """Tool to perform calculations."""
-    try:
-        # Evaluate the mathematical expression provided in the query   
-        result = x + y# if user ask for subtr
+    """Tool to perform addition calculations."""
+    try: 
+        result = x + y
         print("Calculator_tool called with")
         return f"The result of adding {x} and {y} is {result}."
     except Exception as e:
@@ -46,26 +42,10 @@ def start_agent(agent_executor):
         print()
 
 def main():
-
-    # initialize a chat model
     model = ChatOpenAI(temperature=0)
-
-    # tools for the agent 
-    tools = []
-
-
-    # initialize an agent with the chat model
+    tools = [calculator_tool]
     agent_executor = create_agent(model, tools=tools)
 
     start_agent(agent_executor)
 if __name__ == "__main__":
     main()
-
-
-# REAct
-# I need to find the the number of kids in each grade in the school
-# Action: calculator_tool
-# Action Input: "What is 30 kids in grade 1 + 25 kids in grade 2 + 20 kids in grade 3?"
-# Observation: "The result of adding 30 and 25 is 55. The result of adding 55 and 20 is 75."
-# Thought: I now know the total number of kids in the school
-# Final Answer: "The total number of kids in the school is 75."
